80d06e26711bef99bf2f30b323e32574010e33b7
amen <pawel.reclaw@gmail.com>
2020-11-14 14:05:37 +0100

    first commit

--- src/main/java/com/sda/javagdy4/spring/students_demo/controller/MultiplicationController.java
+++ src/main/java/com/sda/javagdy4/spring/students_demo/controller/MultiplicationController.java
@@ -2,10 +2,7 @@ package com.sda.javagdy4.spring.students_demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,8 +15,13 @@ public class MultiplicationController {
     // 2. attributes
     // 3. mappings
 
+
+    // #################
+    // # REQUEST PARAM #
+    // #################
+
     // localhost:8080/
-    // localhost:8080/?imie=baca -> Welcome baca!
+    // localhost:8080/?imie=baca -> Welcome baca! - request param
 //    @RequestMapping(path = "/", method = RequestMethod.GET)
     @GetMapping("/")
     public String indexPage(Model model, @RequestParam(name = "imie", required = false) String imie){
@@ -27,6 +29,37 @@ public class MultiplicationController {
 
         return "index";
     }
+
+
+    // #################
+    // # PATH VARIABLE #
+    // #################
+    // localhost:8080/welcome/     - Heloł heloł maj frend!
+    // localhost:8080/welcome/baca -> Welcome baca!
+    //
+    @GetMapping("/welcome/{imie_param}")
+    public String indexPagePathVariable(Model model, @PathVariable(name = "imie_param", required = false) String imie){
+        model.addAttribute("moje_imie", imie);
+
+        return "index";
+    }
     
-    
+    // #################
+    // # Multiply fORM #
+    // #################
+    // GET - wyświetl formularz
+    @GetMapping("/multiply")
+    public String multiplyForm(){
+        return "multiplication";
+    }
+
+    // POST - odbierz dane z wypełnionego formularza i wyświelt tabliczkę mnożenia
+    @PostMapping("/multiply")
+    public String multiply(Model model,
+            @RequestParam(name="rows", defaultValue = "10") int rows, @RequestParam(name="cols", defaultValue = "10") int cols){
+        model.addAttribute("rowNum", rows);
+        model.addAttribute("colNum", cols);
+
+        return "multiplication";
+    }
 }
